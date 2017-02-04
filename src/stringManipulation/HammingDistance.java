3cc1d3adf7a2e8b13351095f73a92d6015a8f9cf
Prateek <Prateek@10.0.0.82>
2017-02-04 09:47:20 -0800

    HammingDistance

---
+++ src/stringManipulation/HammingDistance.java
@@ -0,0 +1,18 @@
+package stringManipulation;
+
+public class HammingDistance {
+
+	public static void main(String[] args) {
+		System.out.println(hammingDistance(1, 4));
+	}
+	
+	public static int hammingDistance(int x, int y) {
+        int xor = x^y;
+        int res = 0;
+        while(xor!=0) {
+            res+= xor & 1;
+            xor >>= 1;
+        }
+        return res;
+    }
+}
